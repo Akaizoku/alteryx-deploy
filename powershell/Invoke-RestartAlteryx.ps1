ad8c54ad03f88c828db0bacb57961972b10a3c50
Florian <Akaizoku@users.noreply.github.com>
2021-09-03 14:06:44 +0100

    Create Invoke-RestartAlteryx.ps1

---
+++ powershell/Invoke-RestartAlteryx.ps1
@@ -0,0 +1,41 @@
+function Invoke-RestartAlteryx {
+    <#
+        .SYNOPSIS
+        Restart Alteryx Server
+
+        .DESCRIPTION
+        Restart the Alteryx Server service
+
+        .NOTES
+        File name:      Invoke-RestartAlteryx.ps1
+        Author:         Florian Carrier
+        Creation date:  2021-08-27
+        Last modified:  2021-08-30
+    #>
+    [CmdletBinding ()]
+    Param (
+        [Parameter (
+        Position    = 1,
+        Mandatory   = $true,
+        HelpMessage = "Script properties"
+        )]
+        [ValidateNotNullOrEmpty ()]
+        [System.Collections.Specialized.OrderedDictionary]
+        $Properties,
+        [Parameter (
+            HelpMessage = "Non-interactive mode"
+        )]
+        [Switch]
+        $Unattended
+    )
+    Begin {
+        # Get global preference vrariables
+        Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    }
+    Process {
+        Write-Log -Type "CHECK" -Message "Restarting Alteryx Service"
+        Invoke-StopAlteryx  -Properties $Properties -Unattended:$Unattended
+        Invoke-StartAlteryx -Properties $Properties -Unattended:$Unattended
+        Write-Log -Type "CHECK" -Message "Alteryx Service restart complete"
+    }
+}
