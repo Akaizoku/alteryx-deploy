996b7f67fc6fd4b0b19eca85d68d4608810474b9
Florian Carrier <Akaizoku@users.noreply.github.com>
2022-06-29 21:50:58 +0200

    Add patch action

--- Deploy-Alteryx.ps1
+++ Deploy-Alteryx.ps1
@@ -15,25 +15,27 @@
 	
     - activate:     activate the Alteryx application license
     - backup:       backup the Alteryx application database
+    - configure:    configure the Alteryx application
     - deactivate:   deactivate the Alteryx application license
-	- install:      install the Alteryx application
-	- restart:      restart the Alteryx application
-	- restore:      restore a backup of the Alteryx application database
-	- show:         display the script configuration
-	- start:        start the Alteryx application
-	- stop:         stop the Alteryx application
-	- uninstall:    uninstall the Alteryx application
-	- upgrade:      upgrade the Alteryx application
+    - install:      install the Alteryx application
+    - path:         patch upgrade the Alteryx application
+    - restart:      restart the Alteryx application
+    - restore:      restore a backup of the Alteryx application database
+    - show:         display the script configuration
+    - start:        start the Alteryx application
+    - stop:         stop the Alteryx application
+    - uninstall:    uninstall the Alteryx application
+    - upgrade:      upgrade the Alteryx application
 
 	.PARAMETER Unattended
 	The unattended switch define if the script should run in silent mode without any user interaction.
 
-	.NOTES
-	File name:      Deploy-Alteryx.ps1
-	Author:         Florian Carrier
-	Creation date:  2021-06-13
-	Last modified:  2021-11-15
-	Dependencies:   - PowerShell Tool Kit (PSTK)
+    .NOTES
+    File name:      Deploy-Alteryx.ps1
+    Author:         Florian Carrier
+    Creation date:  2021-06-13
+    Last modified:  2022-06-29
+    Dependencies:   - PowerShell Tool Kit (PSTK)
                     - Alteryx PowerShell Module (PSAYX)
 
 	.LINK
@@ -52,27 +54,29 @@
 
 # Static parameters
 Param (
-	[Parameter (
-		Position    = 1,
-		Mandatory   = $false,
-		HelpMessage = "Action to perform"
-	)]
-	[ValidateSet (
-		"activate",
-		"backup",
+    [Parameter (
+        Position    = 1,
+        Mandatory   = $true,
+        HelpMessage = "Action to perform"
+    )]
+    [ValidateSet (
+        "activate",
+        "backup",
+        "configure",
         "deactivate",
-		"install",
-		"restart",
-		"restore",
-		"show",
-		"start",
-		"stop",
-		"uninstall",
-		"upgrade"
-	)]
-	[System.String]
-	$Action,
-	[Parameter (
+        "install",
+        "patch",
+        "restart",
+        "restore",
+        "show",
+        "start",
+        "stop",
+        "uninstall",
+        "upgrade"
+    )]
+    [System.String]
+    $Action,
+    [Parameter (
 		Position	= 2,
 		Mandatory	= $false,
 		HelpMessage	= "Version parameter overwrite"
@@ -243,8 +247,10 @@ Process {
 	switch ($Action) {
         "activate"      { Invoke-ActivateAlteryx    -Properties $Properties -Unattended:$Unattended                                                 }
         "backup"        { Invoke-BackupAlteryx      -Properties $Properties -Unattended:$Unattended                                                 }
+        "configure"     { Set-Configuration         -Properties $Properties -Unattended:$Unattended                                                 }
         "deactivate"    { Invoke-DeactivateAlteryx  -Properties $Properties -Unattended:$Unattended                                                 }
         "install"       { Install-Alteryx           -Properties $Properties -InstallationProperties $InstallationProperties -Unattended:$Unattended }
+        "patch"         { Invoke-PatchAlteryx       -Properties $Properties -Unattended:$Unattended                                                 }
         "restart"       { Invoke-RestartAlteryx     -Properties $Properties -Unattended:$Unattended                                                 }
         "restore"       { Invoke-RestoreAlteryx     -Properties $Properties -Unattended:$Unattended                                                 }
         "show"          { Show-Configuration        -Properties $Properties -InstallationProperties $InstallationProperties                         }
